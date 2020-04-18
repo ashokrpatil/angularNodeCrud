a804f4c5c7d8e716bcb96c7aabe2c0f35e07cd38
ashokrpatil <ashokrpatil_engg@yahoo.co.in>
2020-04-18 18:11:54 +0530

    search coulmn in table

--- Angular/src/app/gst-get/gst-get.component.ts
+++ Angular/src/app/gst-get/gst-get.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 import {Business} from '../Business';
 import { BusinessService } from '../business.service';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-gst-get',
@@ -10,8 +11,16 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 })
 
 export class GstGetComponent implements OnInit {
+
+  itemcode= new FormControl ("");
+  filterValues = {
+    item_code: ''
+  };
   displayedColumns = ['item_code', 'vendor_code','plant_code','edit','delete',
   'abcd','def','abcd','abcd','abcd','def','abcd','abcd','abcd','def','abcd','abcd'];
+  itemRecords: void;
+  copybusiness: any;
+  tData: MatTableDataSource<any>;
   // businesses: MatTableDataSource<Business>;
 
     // businesses= new MatTableDataSource<Business>(data);
@@ -26,12 +35,29 @@ export class GstGetComponent implements OnInit {
 
     // this.businesses.paginator = this.paginator;
 
-    this.bs
-      .getBusinesses()
-      .subscribe((data: Business[]) => {
-        this.businesses = data;
+this.getData();
+
+    this.itemcode.valueChanges
+    .subscribe(item_code => {
+      this.filterValues.item_code = item_code
+      this.tData.filter = JSON.stringify(this.filterValues)
     });
   }
+
+getData(){
+  this.bs
+  .getBusinesses()
+  .subscribe((data: Business[]) => {
+    this.businesses = data;
+    console.log( this.businesses);
+    this.tData = new MatTableDataSource<any>(this.businesses);
+    this.tData.filterPredicate = (data, filter: string) => {
+      let serchTerms=JSON.parse(filter);
+      return (!data.item_code || data.item_code.toLowerCase().indexOf(serchTerms.item_code.toLowerCase()) != -1);
+    }
+    
+});
+}
 
   deleteBusiness(id) {
     this.bs.deleteBusiness(id).subscribe(res => {
